81d6df6539cf931470138bc361a66cdef11b0537
kennethryandy <dykennethryan@gmail.com>
2025-07-30 05:59:12 +0800

    gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -146,6 +146,7 @@ export default function Gallery() {
 			</div>
 
 			{/* Wallpaper Gallery Grid */}
+			<Gallery />
 			<main className="py-8">
 				<div className="container mx-auto px-4">
 					<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
